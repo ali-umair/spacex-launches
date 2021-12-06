2ad13181daf6cbe7001334faca9afc4c8f90dd4a
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-06 14:19:40 +0500

    Added more information for next launch

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ fetch('https://api.spacex.land/rest/launch-next')
 .then(response => response.json())
 .then(data => {
     // const parsedData = JSON.parse(data);
-    const p = document.querySelector('p');
-    p.textContent = data.id;
+    const id = document.querySelector('.id-latest');
+    const date = document.querySelector('.date-latest');
+    const site = document.querySelector('.site-latest');
+    const details = document.querySelector('.details-latest');
+    id.textContent = data.id;
+    date.textContent = data.launch_date_utc;
+    site.textContent = data.launch_site.site_name_long;
+    details.textContent = data.details;
 
-    console.log(data.id);
+
+    // console.log(data.id);
 });
