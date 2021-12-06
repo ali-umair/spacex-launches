9149726b3e6e25cdd6b4fedc687692aa410160e7
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-06 15:06:20 +0500

    Added patch and some styling

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ fetch('https://api.spacex.land/rest/launch-next')
     const date = document.querySelector('.date-latest');
     const site = document.querySelector('.site-latest');
     const details = document.querySelector('.details-latest');
+    const patch = document.querySelector('img');
     id.textContent = data.id;
     date.textContent = data.launch_date_utc;
     site.textContent = data.launch_site.site_name_long;
     details.textContent = data.details;
+    patch.setAttribute('src', 'https://imgur.com/jHNFSY6.png')
 
 
     // console.log(data.id);
