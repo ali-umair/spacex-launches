ca4d3c25dc90b6100be39d9d41541bd16e5a7cc7
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-06 14:02:35 +0500

    Test successful

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ fetch('https://api.spacex.land/rest/launch-next')
 .then(response => response.json())
 .then(data => {
     // const parsedData = JSON.parse(data);
+    const p = document.querySelector('p');
+    p.textContent = data.id;
 
     console.log(data.id);
 });
