33c78df74ae6dd4710bd878c489dc904ce4bad13
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-07 13:29:00 +0500

    Changed directory structure

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ fetch('https://api.spacex.land/rest/launch-next')
 .then(response => response.json())
 .then(data => {
     // const parsedData = JSON.parse(data);
-    const id = document.querySelector('.id-latest');
+    const name = document.querySelector('.name-latest');
     const date = document.querySelector('.date-latest');
     const site = document.querySelector('.site-latest');
     const details = document.querySelector('.details-latest');
     const patch = document.querySelector('img');
-    id.textContent = data.id;
+    name.textContent = data.mission_name;
     date.textContent = data.launch_date_utc;
     site.textContent = data.launch_site.site_name_long;
     details.textContent = data.details;
