137663484964ad7f8b7fdf8470122ace33a317e9
umair-ali14 <17-CP-62@students.uettaxila.edu.pk>
2021-12-21 14:46:57 +0500

    Test successful - Swing

--- js/detail-latest.js
+++ js/detail-latest.js
@@ -2,102 +2,113 @@ fetch('https://api.spacex.land/rest/launch-next')
 .then(response => response.json())
 .then(data => {
 
-    dataHandler(data);
+    renderTemplate(data);
 
 });
 
 
-function nodeHandler() {
-
-    const title = document.querySelector('title');
-    const h1 = document.querySelector('h1');
-    const rocketName = document.querySelector('.rocket-name');
-    const rocketId = document.querySelector('.rocket-id');
-    const rocketType = document.querySelector('.rocket-type');
-    const cores = document.querySelector('.cores');
-    const block = document.querySelector('.block');
-    const flight = document.querySelector('.flight');
-    const gridfins = document.querySelector('.gridfins');
-    const landingIntent = document.querySelector('.landing-intent');
-    const landingType = document.querySelector('.landing-type');
-    const legs = document.querySelector('.legs');
-    const reused = document.querySelector('.reused');
-    const coreId = document.querySelector('.core-id');
-    const coreBlock = document.querySelector('.core-block');
-    const asdsAttempts = document.querySelector('.asds-attempts');
-    const asdsLandings = document.querySelector('.asds-landings');
-    const coreReuseCount = document.querySelector('.core-reuse-count');
-    const previousMissionCount = document.querySelector('.previous-missions-count');
-    const previousMissionName = document.querySelector('.previous-missions-name');
-    const previousMissionFlight = document.querySelector('.previous-missions-flight');
-
-    return [
-        title, h1, rocketName, rocketId, rocketType, cores, block, flight, gridfins, 
-        landingIntent, landingType, legs, reused, coreId, coreBlock, asdsAttempts, asdsLandings,
-        coreReuseCount, previousMissionCount, previousMissionName, previousMissionFlight
-    ];
+function renderTemplate(data) {
+    let body = document.querySelector('body');
+    let template = document.querySelector('#template');
+    let templateHTML = template.textContent;
+    let renderedTemplate = swig.compile(templateHTML);
+    let result = renderedTemplate({data: data});
+    body.innerHTML = result;
+    
 }
 
 
-function dataHandler(data) {
+// function nodeHandler() {
 
-    const nodes = nodeHandler();
+//     const title = document.querySelector('title');
+//     const h1 = document.querySelector('h1');
+//     const rocketName = document.querySelector('.rocket-name');
+//     const rocketId = document.querySelector('.rocket-id');
+//     const rocketType = document.querySelector('.rocket-type');
+//     const cores = document.querySelector('.cores');
+//     const block = document.querySelector('.block');
+//     const flight = document.querySelector('.flight');
+//     const gridfins = document.querySelector('.gridfins');
+//     const landingIntent = document.querySelector('.landing-intent');
+//     const landingType = document.querySelector('.landing-type');
+//     const legs = document.querySelector('.legs');
+//     const reused = document.querySelector('.reused');
+//     const coreId = document.querySelector('.core-id');
+//     const coreBlock = document.querySelector('.core-block');
+//     const asdsAttempts = document.querySelector('.asds-attempts');
+//     const asdsLandings = document.querySelector('.asds-landings');
+//     const coreReuseCount = document.querySelector('.core-reuse-count');
+//     const previousMissionCount = document.querySelector('.previous-missions-count');
+//     const previousMissionName = document.querySelector('.previous-missions-name');
+//     const previousMissionFlight = document.querySelector('.previous-missions-flight');
 
-    // const n = () => {
-    //     let i = 0;
-    //     for(let i=0; i<=16; i++){
-    //         return i;   
-    //     }
-    //     return i;
-    // }
+//     return [
+//         title, h1, rocketName, rocketId, rocketType, cores, block, flight, gridfins, 
+//         landingIntent, landingType, legs, reused, coreId, coreBlock, asdsAttempts, asdsLandings,
+//         coreReuseCount, previousMissionCount, previousMissionName, previousMissionFlight
+//     ];
+// }
 
-    // console.log(n);
 
-    nodes[0].textContent = `${data.mission_name} Details`;
+// function dataHandler(data) {
 
-    nodes[1].textContent = `${data.mission_name} Details`;
+//     const nodes = nodeHandler();
 
-    nodes[2].textContent = data.rocket.rocket_name;
+//     // const n = () => {
+//     //     let i = 0;
+//     //     for(let i=0; i<=16; i++){
+//     //         return i;   
+//     //     }
+//     //     return i;
+//     // }
 
-    nodes[3].textContent = data.rocket.rocket.id;
+//     // console.log(n);
 
-    nodes[4].textContent = data.rocket.rocket_type;
+//     nodes[0].textContent = `${data.mission_name} Details`;
 
-    nodes[5].textContent = `Cores: ${Object.keys(data.rocket.first_stage).length}`;
+//     nodes[1].textContent = `${data.mission_name} Details`;
 
-    nodes[6].textContent = data.rocket.first_stage.cores[0].block;
+//     nodes[2].textContent = data.rocket.rocket_name;
 
-    nodes[7].textContent = data.rocket.first_stage.cores[0].flight;
+//     nodes[3].textContent = data.rocket.rocket.id;
 
-    nodes[8].textContent = data.rocket.first_stage.cores[0].gridfins;
+//     nodes[4].textContent = data.rocket.rocket_type;
 
-    nodes[9].textContent = data.rocket.first_stage.cores[0].landing_intent;
+//     nodes[5].textContent = `Cores: ${Object.keys(data.rocket.first_stage).length}`;
 
-    nodes[10].textContent = data.rocket.first_stage.cores[0].landing_type;
+//     nodes[6].textContent = data.rocket.first_stage.cores[0].block;
 
-    nodes[11].textContent = data.rocket.first_stage.cores[0].legs;
+//     nodes[7].textContent = data.rocket.first_stage.cores[0].flight;
 
-    nodes[12].textContent = data.rocket.first_stage.cores[0].reused;
+//     nodes[8].textContent = data.rocket.first_stage.cores[0].gridfins;
 
-    nodes[13].textContent = data.rocket.first_stage.cores[0].core.id;
+//     nodes[9].textContent = data.rocket.first_stage.cores[0].landing_intent;
 
-    nodes[14].textContent = data.rocket.first_stage.cores[0].core.block;
+//     nodes[10].textContent = data.rocket.first_stage.cores[0].landing_type;
 
-    nodes[15].textContent = data.rocket.first_stage.cores[0].core.asds_attempts;
+//     nodes[11].textContent = data.rocket.first_stage.cores[0].legs;
 
-    nodes[16].textContent = data.rocket.first_stage.cores[0].core.asds_landings;
+//     nodes[12].textContent = data.rocket.first_stage.cores[0].reused;
 
-    nodes[17].textContent = data.rocket.first_stage.cores[0].core.reuse_count;
+//     nodes[13].textContent = data.rocket.first_stage.cores[0].core.id;
 
-    console.log(data.rocket.first_stage.cores[0].core.missions[0].name);
+//     nodes[14].textContent = data.rocket.first_stage.cores[0].core.block;
 
-    nodes[18].textContent = data.rocket.first_stage.cores[0].core.missions.length;
+//     nodes[15].textContent = data.rocket.first_stage.cores[0].core.asds_attempts;
 
-    nodes[19].textContent = `"${data.rocket.first_stage.cores[0].core.missions[0].name}" - 
-    "${data.rocket.first_stage.cores[0].core.missions[1].name}" - 
-    "${data.rocket.first_stage.cores[0].core.missions[2].name}"`;
+//     nodes[16].textContent = data.rocket.first_stage.cores[0].core.asds_landings;
 
-    // nodes[20].textContent = data.rocket.first_stage.cores[0].core.reuse_count;
+//     nodes[17].textContent = data.rocket.first_stage.cores[0].core.reuse_count;
 
-    // console.log(data);
-}
+//     console.log(data.rocket.first_stage.cores[0].core.missions[0].name);
+
+//     nodes[18].textContent = data.rocket.first_stage.cores[0].core.missions.length;
+
+//     nodes[19].textContent = `"${data.rocket.first_stage.cores[0].core.missions[0].name}" - 
+//     "${data.rocket.first_stage.cores[0].core.missions[1].name}" - 
+//     "${data.rocket.first_stage.cores[0].core.missions[2].name}"`;
+
+//     // nodes[20].textContent = data.rocket.first_stage.cores[0].core.reuse_count;
+
+//     // console.log(data);
+// }
